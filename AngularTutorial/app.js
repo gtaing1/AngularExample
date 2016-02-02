8b6f7c0522fcce68769dd85ccbdb0c86aca24695
Hector Rios <that.hector@gmail.com>
2016-02-01 20:21:20 -0600

    Added new controllers. Ended on page 96

--- AngularTutorial/app.js
+++ AngularTutorial/app.js
@@ -6,7 +6,24 @@
     });
 
     app.controller('PanelController', function () {
+        this.tab = 1;
 
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller('ReviewController', function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
     });
 
     var gems = [{
@@ -20,6 +37,18 @@
                 full: 'http://img.pokemondb.net/artwork/steelix.jpg',
                 thumb: 'http://img.pokemondb.net/artwork/steelix.jpg'
             }
+        ],
+        reviews: [
+            {
+                stars: 5,
+                body: "This is the best pokemon EVA!",
+                author: "someguy@gmail.com"
+            },
+            {
+                stars: 3,
+                body: "This Pokemon wasn't very effective against fire",
+                author: "youngster_joey@yahoo.com"
+            }
         ]
     },
     {
@@ -27,11 +56,23 @@
         description: 'Not only is this a great Pokemon game, this is also an AWESOME programming language. 10/10',
         price: 0.0,
         canPurchase: true,
-        soldOut: false,
+        soldOut: true,
         images: [
             {
                 full: 'http://nicholasjohnson.com/images/sections/ruby.png',
                 thumb: 'http://nicholasjohnson.com/images/sections/ruby.png'
+            }
+        ],
+        reviews: [
+            {
+                stars: 4,
+                body: "This is THE programming language!",
+                author: "someguy@gmail.com"
+            },
+            {
+                stars: 1,
+                body: "It's trying to be Python... No way!",
+                author: "guido@gnu.org"
             }
         ]
     },
@@ -45,6 +86,18 @@
             {
                 full: 'https://i.ytimg.com/vi/jL2vM32-b1A/hqdefault.jpg',
                 thumb: 'https://i.ytimg.com/vi/jL2vM32-b1A/hqdefault.jpg'
+            }
+        ],
+        reviews: [
+            {
+                stars: 5,
+                body: "This is the best pokemon EVA!",
+                author: "someguy@gmail.com"
+            },
+            {
+                stars: 3,
+                body: "Who is this guy? Why isn't this like LUGIA?!",
+                author: "youngster_joey@yahoo.com"
             }
         ]
     }];
